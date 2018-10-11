a9d8b6f7a043f8dc338f9c93a0647fd98069275b
Raju Karri <rajukarri0508@gmail.com>
2018-10-11 22:42:23 +0530

    mod app.js

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require("express");
 var app     = express();
 app.set('view engine' , 'ejs');
 app.get( "/" , function(req,res){
+    console.log("this is console");
+    res.send("Welcom to home page");
+});
+app.get("/raju" , function(req,res){
+    console.log("this is console");
     res.send("My Name is Raju");
 });
 app.listen(process.env.PORT,process.env.IP);
